25900070442d63d3af0fd2b49d7c56e85beb614b
Astrid <astrid.jimenez.barrera@uabc.edu.mx>
2025-05-22 09:28:04 -0700

    Update Plataforma.java

--- Plataforma.java
+++ Plataforma.java
@@ -1,12 +1,26 @@
 import java.awt.*;
+import javax.swing.ImageIcon;
 
 public class Plataforma extends Entidad {
+    private Image imagen;
+
     public Plataforma(int x, int y, int ancho, int alto) {
         super(x, y, ancho, alto);
+        try {
+            ImageIcon icono = new ImageIcon("plataforma.jpg");
+            imagen = icono.getImage().getScaledInstance(ancho, alto, Image.SCALE_SMOOTH);
+        } catch (Exception e) {
+            System.out.println("Error cargando imagen de plataforma");
+            imagen = null;
+        }
     }
 
     public void dibujar(Graphics g) {
-        g.setColor(Color.DARK_GRAY);
-        g.fillRect(x, y, ancho, alto);
+        if (imagen != null) {
+            g.drawImage(imagen, x, y, null);
+        } else {
+            g.setColor(Color.DARK_GRAY);
+            g.fillRect(x, y, ancho, alto);
+        }
     }
 }
