263e0ae7d5b1534b7b288bf2e8a0787b3ef2d89c
Astrid <astrid.jimenez.barrera@uabc.edu.mx>
2025-05-22 09:28:27 -0700

    Update EnemigoVolador.java

--- EnemigoVolador.java
+++ EnemigoVolador.java
@@ -4,14 +4,28 @@ import java.awt.Image;
 
 public class EnemigoVolador extends Enemigo {
     private Image imagen;
-    public EnemigoVolador(int x, int y, int ancho, int alto) {
+    private int velocidadY = 5; // Velocidad de caída
 
+    public EnemigoVolador(int x, int y, int ancho, int alto) {
         super(x, y, ancho, alto);
         ImageIcon icono = new ImageIcon("sierra.png");
         imagen = icono.getImage().getScaledInstance(ancho, alto, Image.SCALE_SMOOTH);
     }
 
+    public void actualizar() {
+        y += velocidadY;  // La sierra cae hacia abajo
+    }
+
     public void dibujar(Graphics g) {
         g.drawImage(imagen, x, y, null);
     }
+
+    public boolean estaFueraPantalla(int alturaPantalla) {
+        return y > alturaPantalla;
+    }
+
+    public Rectangle getBounds() {
+        return new Rectangle(x, y, ancho, alto);
+    }
+
 }
