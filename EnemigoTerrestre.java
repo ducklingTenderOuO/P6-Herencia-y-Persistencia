b20a5e7e19d5e14cc277c082d8f84c6d7f773ccc
LennyMX <patron.luis@uabc.edu.mx>
2025-05-21 21:10:54 -0700

    Update EnemigoTerrestre.java

--- EnemigoTerrestre.java
+++ EnemigoTerrestre.java
@@ -1,26 +1,8 @@
 import java.awt.*;
 
 public class EnemigoTerrestre extends Enemigo {
-    private int velocidad = 2;
-
-    public EnemigoTerrestre(int x, int y, int ancho, int alto, int vida) {
-        super(x, y, ancho, alto, vida);
-    }
-
-    @Override
-    public void morir() {
-        System.out.println("El enemigo terrestre ha muerto");
-    }
-
-    public void actualizar(Jugador jugador) {
-        int dx = jugador.getX() - x;
-        int dy = jugador.getY() - y;
-        double distancia = Math.sqrt(dx * dx + dy * dy);
-
-        if (distancia != 0) {
-            x += (int)(velocidad * dx / distancia);
-            y += (int)(velocidad * dy / distancia);
-        }
+    public EnemigoTerrestre(int x, int y, int ancho, int alto) {
+        super(x, y, ancho, alto);
     }
 
     public void dibujar(Graphics g) {
